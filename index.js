b95ab806eb38f91b75b5cdc28f82f7ebdf0f476d
altmattr <alt.mattr@gmail.com>
2020-12-05 10:19:46 +0000

    better starting point

--- index.js
+++ index.js
@@ -6,19 +6,7 @@ var ops = require('rxjs/operators');
    in this input data might be separated by newlines or spaces
 */
 exports.part1 = function(data){
-  return rx.from(data).pipe(
-    ops.filter(r => r != ""),
-    ops.map(r => {
-      obj = {}
-      r.replace(/\n/g, ' ').split(' ').forEach(str => {
-        var parts = str.split(":");
-        obj[parts[0]] = parts[1];
-      });
-      return obj;
-    }),
-    ops.filter(o => o.byr && o.iyr && o.eyr && o.hgt && o.hcl && o.ecl && o.pid),
-    ops.count()
-  );
+  return rx.from([]);
 }
 
 /* data arrives as an array of passport data from the input file.  Each slot
@@ -26,43 +14,5 @@ exports.part1 = function(data){
    in this input data might be separated by newlines or spaces
 */
 exports.part2 = function(data){
-  return rx.from(data).pipe(
-    ops.filter(r => r != ""),
-    ops.map(r => {
-      obj = {}
-      r.replace(/\n/g, ' ').split(' ').forEach(str => {
-        var parts = str.split(":");
-        obj[parts[0]] = parts[1];
-      });
-      return obj;
-    }),
-    ops.filter(o => o.byr && o.iyr && o.eyr && o.hgt && o.hcl && o.ecl && o.pid),
-    ops.filter(o => {
-      byr = parseInt(o.byr);
-      return (byr && byr >=1920 && byr <=2002)
-    }),
-    ops.filter(o => {
-      iyr = parseInt(o.iyr);
-      return (iyr && iyr >=2010 && iyr <=2020)
-    }),
-    ops.filter(o => {
-      eyr = parseInt(o.eyr);
-      return (eyr && eyr >=2020 && eyr <=2030)
-    }),
-    ops.filter(o => {
-      val = parseInt(o.hgt.substring(0, o.hgt.length-2));
-      unit = o.hgt.substring(o.hgt.length-2, o.hgt.length);
-      if(unit == "cm"){
-        return val >=150 && val <=193;
-      }else if (unit == "in"){
-        return val >=59 && val <=76;
-      }else {
-        return false;
-      }
-    }),
-    ops.filter(o => o.hcl.match(/^\#[0123456789abcdef]{6}$/)),
-    ops.filter(o => (o.ecl == "amb" || o.ecl == "blu" || o.ecl == "brn" || o.ecl == "gry" || o.ecl == "grn" || o.ecl == "hzl" || o.ecl == "oth")),
-    ops.filter(o => o.pid.match(/^[0123456789]{9}$/)),
-    ops.count()
-  );
+  return rx.from([]);
 }
